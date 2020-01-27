505e3b9a2ef6504e7490420daec9b219ba45e33e
Samuel Vandak <48286115+NomDeiX@users.noreply.github.com>
2020-01-27 19:49:33 +0100

    Update GIFanimation.py

--- GIFanimation.py
+++ GIFanimation.py
@@ -1,34 +1,10 @@
-import tkinter
-w = 1280
-h = 720
-canvas = tkinter.Canvas(width=w,height=h,bg="#71CAE7")
-canvas.pack()
-from random import *
 import datetime
-import time
 import os
 
-
-num = 0
-arr = []
-subor = open("TRANSAKCIE_PAYWALL.txt", "a+")
-subor.seek(0)
-
-
-num = subor.readline().strip()
-for i in range (int(num)):
-    riadok = subor.readline()
-    arr.append(riadok.strip())
+path= "test.txt"
+subor = open(path,"r+")
+print(subor.readline())
+subor.close()
 
 
-
-def newfile():
-    global num, arr,subor
-    subor.close()
-    novysubor = open("TRANSAKCIE_PAYWALL.txt", "w+")
-    novysubor.write(str(int(num)+1))
-    for i in range (len(arr)):
-        novysubor.write("\n" + arr[i])
-    novysubor.write("\n" + "nieconieconieco")
-
-
+#os.remove(subor)
